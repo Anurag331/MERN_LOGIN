9e82365f74efb52901090f58930f32ee95dab02b
Anurag331 <anuragchaurasiaexam@gmail.com>
2022-09-15 00:02:08 +0530

    complete project

---
+++ Client/src/components/Home.js
@@ -0,0 +1,47 @@
+import React, { useState,useEffect } from 'react'
+import Cookies from 'js-cookie';
+
+function Home() {
+
+    const [UserName,setUserName]=useState('');
+    const [show,setShow]=useState(false);
+    const userHomePage = async () => {
+        try {
+          const jwt=Cookies.get('jwt');
+          const res = await fetch("http://localhost:5000/getdata", {
+            method: "GET",
+            headers: {
+              "Authorization":"Bearer " +jwt,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
+          const data = await res.json();
+          console.log(data);
+          setUserName(data.name);
+          setShow(true);
+          
+         
+        } catch (err) {
+          console.log(err);
+        }
+      };
+    
+      useEffect(() => {
+          userHomePage();
+      },[]);
+
+    return (
+     <>
+      
+        <div class="middle">
+            <p>WELCOME {UserName}</p>
+            <h1>{show?"Happy to See You Back":"Please Login To View Details"}</h1>
+        </div>
+      
+       </>
+    )
+}
+
+export default Home
